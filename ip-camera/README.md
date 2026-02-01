9820cc21e425b759dbd38e7455d6d6d717147302
kiddos <kiddo831007@gmail.com>
2026-02-01 11:30:24 +0800

    ip-camera

---
+++ ip-camera/README.md
@@ -0,0 +1,2 @@
+Component config -> ESP PSRAM -> SPI RAM access method
+Make RAM allocatable using malloc() as well
---
+++ ip-camera/main/main.c
@@ -0,0 +1,363 @@
+#include "esp_camera.h"
+#include "esp_chip_info.h"
+#include "esp_event.h"
+#include "esp_flash.h"
+#include "esp_http_server.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "freertos/event_groups.h"
+#include "freertos/task.h"
+#include "mdns.h"
+#include "nvs_flash.h"
+#include "sdkconfig.h"
+#include <stdio.h>
+
+#if CONFIG_ESP_WPA3_SAE_PWE_HUNT_AND_PECK
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HUNT_AND_PECK
+#define EXAMPLE_H2E_IDENTIFIER ""
+#elif CONFIG_ESP_WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_HASH_TO_ELEMENT
+#define EXAMPLE_H2E_IDENTIFIER CONFIG_ESP_WIFI_PW_ID
+#elif CONFIG_ESP_WPA3_SAE_PWE_BOTH
+#define ESP_WIFI_SAE_MODE WPA3_SAE_PWE_BOTH
+#endif
+#if CONFIG_ESP_WIFI_AUTH_OPEN
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_OPEN
+#elif CONFIG_ESP_WIFI_AUTH_WEP
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WEP
+#elif CONFIG_ESP_WIFI_AUTH_WPA_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA_WPA2_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA_WPA2_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WPA2_WPA3_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WPA2_WPA3_PSK
+#elif CONFIG_ESP_WIFI_AUTH_WAPI_PSK
+#define ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD WIFI_AUTH_WAPI_PSK
+#endif
+
+#define CAM_PIN_PWDN 32
+#define CAM_PIN_RESET -1 // software reset will be performed
+#define CAM_PIN_XCLK 0
+#define CAM_PIN_SIOD 26
+#define CAM_PIN_SIOC 27
+
+#define CAM_PIN_D7 35
+#define CAM_PIN_D6 34
+#define CAM_PIN_D5 39
+#define CAM_PIN_D4 36
+#define CAM_PIN_D3 21
+#define CAM_PIN_D2 19
+#define CAM_PIN_D1 18
+#define CAM_PIN_D0 5
+#define CAM_PIN_VSYNC 25
+#define CAM_PIN_HREF 23
+#define CAM_PIN_PCLK 22
+
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+#define MAX(X, Y) (((X) > (Y)) ? (X) : (Y))
+#define WIFI_CONNECTED_BIT BIT0
+#define WIFI_FAIL_BIT BIT1
+
+static const char *TAG = "ip-camera";
+
+static esp_chip_info_t chip_info;
+static camera_config_t camera_config = {
+    .pin_pwdn = CAM_PIN_PWDN,
+    .pin_reset = CAM_PIN_RESET,
+    .pin_xclk = CAM_PIN_XCLK,
+    .pin_sccb_sda = CAM_PIN_SIOD,
+    .pin_sccb_scl = CAM_PIN_SIOC,
+
+    .pin_d7 = CAM_PIN_D7,
+    .pin_d6 = CAM_PIN_D6,
+    .pin_d5 = CAM_PIN_D5,
+    .pin_d4 = CAM_PIN_D4,
+    .pin_d3 = CAM_PIN_D3,
+    .pin_d2 = CAM_PIN_D2,
+    .pin_d1 = CAM_PIN_D1,
+    .pin_d0 = CAM_PIN_D0,
+    .pin_vsync = CAM_PIN_VSYNC,
+    .pin_href = CAM_PIN_HREF,
+    .pin_pclk = CAM_PIN_PCLK,
+
+    // XCLK 20MHz or 10MHz for OV2640 double FPS (Experimental)
+    .xclk_freq_hz = 20000000,
+    .ledc_timer = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_JPEG, // YUV422,GRAYSCALE,RGB565,JPEG
+    .frame_size =
+        FRAMESIZE_VGA, // QQVGA-UXGA, For ESP32, do not use sizes above QVGA
+                        // when not JPEG. The performance of the ESP32-S series
+                        // has improved a lot, but JPEG mode always gives better
+                        // frame rates.
+
+    .jpeg_quality = 10, // 0-63, for OV series camera sensors, lower number
+                        // means higher quality
+    .fb_count = 1, // When jpeg mode is used, if fb_count more than one, the
+                   // driver will work in continuous mode.
+    .fb_location = CAMERA_FB_IN_PSRAM,
+    .grab_mode = CAMERA_GRAB_WHEN_EMPTY,
+};
+static EventGroupHandle_t s_wifi_event_group;
+static int s_retry_num = 0;
+static httpd_handle_t server = NULL;
+
+static httpd_handle_t start_webserver(void);
+static esp_err_t stream_handler(httpd_req_t *req);
+
+static const httpd_uri_t stream = {
+    .uri = "/stream",
+    .method = HTTP_GET,
+    .handler = stream_handler,
+    .user_ctx = NULL,
+};
+
+static httpd_handle_t start_webserver(void) {
+  httpd_handle_t server = NULL;
+  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+  config.lru_purge_enable = true;
+  config.stack_size = 8192;
+
+  // Start the httpd server
+  ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+  if (httpd_start(&server, &config) == ESP_OK) {
+    // Set URI handlers
+    ESP_LOGI(TAG, "Registering URI handlers");
+    httpd_register_uri_handler(server, &stream);
+
+    return server;
+  }
+
+  ESP_LOGI(TAG, "Error starting server!");
+  return NULL;
+}
+
+#define PART_BOUNDARY "123456789000000000000987654321"
+
+static const char *_STREAM_CONTENT_TYPE =
+    "multipart/x-mixed-replace;boundary=" PART_BOUNDARY;
+static const char *_STREAM_BOUNDARY = "\r\n--" PART_BOUNDARY "\r\n";
+static const char *_STREAM_PART =
+    "Content-Type: image/jpeg\r\nContent-Length: %u\r\n\r\n";
+
+static esp_err_t stream_handler(httpd_req_t *req) {
+  camera_fb_t *fb = NULL;
+  esp_err_t res = ESP_OK;
+  char *part_buf[64];
+
+  ESP_LOGI(TAG, "handle stream connection");
+  httpd_resp_set_type(req, _STREAM_CONTENT_TYPE);
+
+  while (true) {
+    fb = esp_camera_fb_get();
+    if (!fb) {
+      ESP_LOGE(TAG, "Camera capture failed");
+      res = ESP_FAIL;
+      break;
+    }
+
+    res =
+        httpd_resp_send_chunk(req, _STREAM_BOUNDARY, strlen(_STREAM_BOUNDARY));
+    if (res == ESP_OK) {
+      size_t hlen = snprintf((char *)part_buf, 64, _STREAM_PART, fb->len);
+      res = httpd_resp_send_chunk(req, (const char *)part_buf, hlen);
+    }
+
+    // ESP_LOGI(TAG, "send image data...");
+    if (res == ESP_OK) {
+      res = httpd_resp_send_chunk(req, (const char *)fb->buf, fb->len);
+    }
+
+    esp_camera_fb_return(fb);
+
+    if (res != ESP_OK) {
+      break;
+    }
+  }
+
+  httpd_resp_send_chunk(req, NULL, 0);
+  return res;
+}
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
+  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+    esp_wifi_connect();
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
+    if (s_retry_num < CONFIG_ESP_MAXIMUM_RETRY) {
+      esp_wifi_connect();
+      s_retry_num++;
+      ESP_LOGI(TAG, "retry to connect to the AP");
+    } else {
+      xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
+    }
+    ESP_LOGI(TAG, "connect to the AP fail");
+  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+    s_retry_num = 0;
+    xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
+
+    if (server == NULL) {
+      ESP_LOGI(TAG, "Starting webserver");
+      server = start_webserver();
+    }
+  }
+}
+
+void init_camera() {
+  esp_err_t err = esp_camera_init(&camera_config);
+  if (err != ESP_OK) {
+    printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+}
+
+esp_err_t wifi_init_sta(void) {
+  s_wifi_event_group = xEventGroupCreate();
+
+  ESP_ERROR_CHECK(esp_netif_init());
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  esp_event_handler_instance_t instance_any_id;
+  esp_event_handler_instance_t instance_got_ip;
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL,
+      &instance_any_id));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL,
+      &instance_got_ip));
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = CONFIG_ESP_WIFI_SSID,
+              .password = CONFIG_ESP_WIFI_PASSWORD,
+              .threshold.authmode = ESP_WIFI_SCAN_AUTH_MODE_THRESHOLD,
+              .sae_pwe_h2e = ESP_WIFI_SAE_MODE,
+              .sae_h2e_identifier = "",
+          },
+  };
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+
+  ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+  /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or
+   * connection failed for the maximum number of re-tries (WIFI_FAIL_BIT). The
+   * bits are set by event_handler() (see above) */
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
+
+  /* xEventGroupWaitBits() returns the bits before the call returned, hence we
+   * can test which event actually happened. */
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(TAG, "connected to ap SSID:%s password:%s", CONFIG_ESP_WIFI_SSID,
+             CONFIG_ESP_WIFI_PASSWORD);
+    return ESP_OK;
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGI(TAG, "Failed to connect to SSID:%s, password:%s",
+             CONFIG_ESP_WIFI_SSID, CONFIG_ESP_WIFI_PASSWORD);
+  } else {
+    ESP_LOGE(TAG, "UNEXPECTED EVENT");
+  }
+  return ESP_FAIL;
+}
+
+static esp_err_t stop_webserver(httpd_handle_t server) {
+  // Stop the httpd server
+  return httpd_stop(server);
+}
+
+static void disconnect_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
+  httpd_handle_t *server = (httpd_handle_t *)arg;
+  if (*server) {
+    ESP_LOGI(TAG, "Stopping webserver");
+    if (stop_webserver(*server) == ESP_OK) {
+      *server = NULL;
+    } else {
+      ESP_LOGE(TAG, "Failed to stop http server");
+    }
+  }
+}
+
+void init_mdns(void) {
+  esp_err_t err = mdns_init();
+  if (err) {
+    printf("mDNS Init failed: %d\n", err);
+    return;
+  }
+
+  ESP_LOGI(TAG, "setting up mdns...");
+  mdns_hostname_set("esp32-ipcamera");
+  mdns_instance_name_set("ESP32 ipcamera");
+  mdns_service_add(NULL, "_http", "_tcp", 80, NULL, 0);
+  ESP_LOGI(TAG, "hostname: esp32-ipcamera");
+}
+
+static void board_info() {
+  /* Print chip information */
+  esp_chip_info(&chip_info);
+  printf("This is %s chip with %d CPU core(s), %s%s%s%s, ", CONFIG_IDF_TARGET,
+         chip_info.cores,
+         (chip_info.features & CHIP_FEATURE_WIFI_BGN) ? "WiFi/" : "",
+         (chip_info.features & CHIP_FEATURE_BT) ? "BT" : "",
+         (chip_info.features & CHIP_FEATURE_BLE) ? "BLE" : "",
+         (chip_info.features & CHIP_FEATURE_IEEE802154)
+             ? ", 802.15.4 (Zigbee/Thread)"
+             : "");
+
+  unsigned major_rev = chip_info.revision / 100;
+  unsigned minor_rev = chip_info.revision % 100;
+  uint32_t flash_size;
+  printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+  if (esp_flash_get_size(NULL, &flash_size) != ESP_OK) {
+    printf("Get flash size failed");
+    return;
+  }
+
+  printf("%" PRIu32 "MB %s flash\n", flash_size / (uint32_t)(1024 * 1024),
+         (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded"
+                                                       : "external");
+
+  printf("Minimum free heap size: %" PRIu32 " bytes\n",
+         esp_get_minimum_free_heap_size());
+}
+
+void app_main(void) {
+  board_info();
+  ESP_ERROR_CHECK(nvs_flash_init());
+
+  while (1) {
+    ESP_LOGI(TAG, "wifi init sta...");
+    ESP_ERROR_CHECK(wifi_init_sta());
+
+    init_mdns();
+    init_camera();
+
+    ESP_LOGI(TAG, "register event handler...");
+    ESP_ERROR_CHECK(esp_event_handler_register(
+        WIFI_EVENT, WIFI_EVENT_STA_DISCONNECTED, &disconnect_handler, &server));
+
+    ESP_LOGI(TAG, "start main loop...");
+    while (server) {
+      vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+    vTaskDelay(100000 / portTICK_PERIOD_MS);
+  }
+}
